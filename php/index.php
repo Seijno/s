920c6dd57d77443a7296975463bbeb7ad749ae85
Seijno <seijnovannieuwkoop@gmail.com>
2022-11-23 11:54:36 +0100

    index pagina met de css)

---
+++ php/index.php
@@ -0,0 +1,28 @@
+<?php include "config.php"; ?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Stripbook</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/style.css">
+  </head>
+  <body>
+    <session id="header">
+      <div class="header" id="header">
+        <div class="container py-3">
+          <div class="row">
+            <div class="d-flex flex-row-reverse">
+              <div class="p-2 text-white"><a class="text-white" href="#">Login</a></div>
+              <div class="p-2 text-white"><a class="text-white" href="#">Cart</a></div>
+              <div class="p-2 text-white"><a class="text-white" href="#">Library</a></div>
+              <div class="p-2 text-white"><a class="text-white" href="#">Shop</a></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </session>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  </body>
+</html>
