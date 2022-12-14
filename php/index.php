ae070672b251f29cf23d0868bbadc5926d47397b
Seijno <seijnovannieuwkoop@gmail.com>
2022-12-14 10:46:12 +0100

    .

--- php/index.php
+++ php/index.php
@@ -29,7 +29,7 @@
       <div class="container">
         <div class="row justify-conent-center">
           <div class="col-12 text-center"><h3></h3></div>
-          <!-- Hier komt content -->
+          <!-- Hier komt content. -->
         </div>
       </div>
     </section>
