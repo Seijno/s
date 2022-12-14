f1a2fdcd3916e567fe5290c8baa05e27a5356344
Seijno <seijnovannieuwkoop@gmail.com>
2022-12-14 10:42:34 +0100

    index en css.

--- php/index.php
+++ php/index.php
@@ -9,20 +9,57 @@
     <link rel="stylesheet" href="../css/style.css">
   </head>
   <body>
-    <session id="header">
-      <div class="header" id="header">
-        <div class="container py-3">
-          <div class="row">
-            <div class="d-flex flex-row-reverse">
-              <div class="p-2 text-white"><a class="text-white" href="#">Login</a></div>
-              <div class="p-2 text-white"><a class="text-white" href="#">Cart</a></div>
-              <div class="p-2 text-white"><a class="text-white" href="#">Library</a></div>
-              <div class="p-2 text-white"><a class="text-white" href="#">Shop</a></div>
+    <section id="header">
+      <div id="header" class="container-fluid py-4">
+        <div class="row">
+        <div class="col-6">
+            <h2>comiclibrary</h2>
+          </div>
+          <div class="col-6 d-flex flex-row-reverse">
+            <div class="p-2"><a href="#">Login</a></div>
+            <div class="p-2"><a href="#">Cart</a></div>
+            <div class="p-2"><a href="#">Library</a></div>
+            <div class="p-2"><a href="#">Shop</a></div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <div class="py-5"></div>
+    <section id="content">
+      <div class="container">
+        <div class="row justify-conent-center">
+          <div class="col-12 text-center"><h3></h3></div>
+          <!-- Hier komt content -->
+        </div>
+      </div>
+    </section>
+    <div class="py-5"></div>
+    <section id="footer">
+      <div class="container">
+        <div class="row justify-content-evenly">
+          <div class="col-4">
+            <div class="text-center">
+              <h6>Nieuwsbrief</h6>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="text-center">
+              <h6>Contact</h6>
+              <p>email: comiclibrary@support.com<br>
+                 telephone number: +31 6 12345678<br>
+                 adres: 2311WV Amsterdam<br>
+                 oosterhoekstraat 37</p>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="text-center">
+              <h6>Navigatie</h6>
+              <a href="#">upcoming</a>
             </div>
           </div>
         </div>
       </div>
-    </session>
+    </section>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   </body>
 </html>
